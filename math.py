a425bb9a0542681829cd8374c3d1fbb86be6a085
Lariosk <105257073+Lariosk@users.noreply.github.com>
2023-02-18 10:38:14 -0600

    Create math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+d= -b*a*c
+
+if d < 0:
+    print("La ecuación no tiene soluciones reales", d)
+    
+
+    Elif d= 0:
+            x == -b/(2*a)
+            print("la solición única es: ", x)
+    Elif d >= 0:
+        x == -b/(2*a)
+        print("la solición única es: ", x)    
+        else:
+            x1 = (-b-(d**(1/2)))/2*a
+            x2 = (b+(d**(1/2)))/2*a
+        print ("las 2 soluciones son:")
+        print(x1)
+        print(x2)
